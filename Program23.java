ad66968e5383e444f4d62d7afa742ab97c43a666
umanggla2002 <95420925+umanggla2002@users.noreply.github.com>
2021-12-13 18:17:26 +0530

    Create Program23.java

---
+++ Program23.java
@@ -0,0 +1,16 @@
+public class display {
+    String a ;
+    void draw(){
+
+    }
+    void erass(){
+
+    }
+}
+class disp{
+public static void main(String []args) {
+
+    System.out.println("Drawing Shape");
+    System.out.println("Erasing shape");
+ }
+}
